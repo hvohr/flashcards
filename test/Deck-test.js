fff197b1713ccc1608034af19fe46acd37b5460f
Hollis Vohr <hollis.vohr@gmail.com>
2023-05-09 13:26:40 -0600

    Re-add testing for deck object

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,17 +1,17 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { createCard, countDeck } = require('../src/card');
+const { createCard, countCards, createDeck } = require('../src/card');
 
 describe('deck', function () {
-// it('should create a deck with an array of card objects', function () {
-// const card1 = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
-// const deck = countDeck(card1)
-// expect(deck).to.equal([{ id: 1, question: 'What allows you to define a set of related information using key-value pairs?', answers: [ 'object', 'array', 'function' ], correctAnswer: 'object'}])
-// });
+it('should create a deck with an array of card objects', function () {
+const card1 = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
+const deck = createDeck(card1)
+expect(deck).to.equal([card1])
+});
 it('should know how many cards are in the deck', function() {
   const card1 = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object')
-  const deckNumber = countDeck(card1)
+  const deckNumber = countCards(card1)
   expect(deckNumber).to.equal(1)
 })
 });
