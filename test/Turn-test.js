03ebe4d9dab0c87ca7da5605b0f2ec4055dab14b
Hollis Vohr <hollis.vohr@gmail.com>
2023-05-08 19:05:34 -0600

    Add turn testing for evaluating user answers

---
+++ test/Turn-test.js
@@ -0,0 +1,24 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const { createCard, evaluateGuess } = require('../src/card');
+
+describe('evaluate the answer', function () {
+
+it('should evaluate guess and return correct response', function () {
+  const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  const guess = 'object'
+  const correctAnswer = card.correctAnswer
+  const evaluate = evaluateGuess(guess, correctAnswer)
+
+  expect(evaluate).to.equal("Correct!")
+});
+it('should evaluate guess and return incorrect response', function () {
+  const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+  const guess = 'array'
+  const correctAnswer = card.correctAnswer
+  const evaluate = evaluateGuess(guess, correctAnswer)
+
+  expect(evaluate).to.equal("Incorrect!")
+});
+});
