caa08bb59280215871bd16f1ff04f5b3083cad59
Hollis Vohr <hollis.vohr@gmail.com>
2023-05-10 20:07:19 -0600

    Repair intro message undefined bug

--- src/game.js
+++ src/game.js
@@ -2,12 +2,13 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
-const { createCard, createRound, evaluateGuess, calculatePercentCorrect } = require('../src/card');
+const {createRound } = require('../src/card');
 const {createDeck, countCards } = require('../src/deck');
 // const {endRound, takeTurn } = require('../src/round');
 
-function printMessage(deck) {
-  console.log(`Welcome to FlashCards! You are playing with ${countCards(deck)} cards.
+function printMessage(deck2) {
+  const round1 = createRound(deck2)
+  console.log(`Welcome to FlashCards! You are playing with ${countCards(round1.deck.cards)} cards.
   -----------------------------------------------------------------------`);
 }
 
@@ -16,16 +17,11 @@ function printQuestion(round) {
 }
 
 function start() {
-  // createCard()
-  // evaluateGuess()
-  var deck1 = createDeck(prototypeQuestions)
+  const deck1 = createDeck(prototypeQuestions)
   printMessage(deck1)
-  // countCards()
-  var round1 = createRound(deck1)
+  const round1 = createRound(deck1)
   printQuestion(round1)
-  // takeTurn()
-  // calculatePercentCorrect()
-  // endRound()
+ 
 }
 
 module.exports = { start, printMessage, printQuestion };
